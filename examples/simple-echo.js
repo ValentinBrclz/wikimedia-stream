0dfc84aede32e8a7785070cd797fe1e180e8b129
Valentin Brclz <valentin@brclz.ch>
2015-11-15 20:25:42 +0200

    Simple echo test

---
+++ examples/simple-echo.js
@@ -0,0 +1,35 @@
+/*
+ * wikimedia-stream - simple-echo.js (example)
+ *
+ * Example that just echo the lines of the channel
+ * Copyright (C) 2015 Valentin Berclaz
+ * <http://www.valentinberclaz.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+var WikimediaStream = require('../lib/wikimedia-stream');
+
+var ws = new WikimediaStream();
+
+try {
+	ws.on("data", function (data) {
+		console.log(data);
+	});
+}
+catch(err)
+{
+	console.log(err);
+}
